7052bc3b07d24faafc3530da7d69a05decdc9461
zxcvos <78369539+zxcvos@users.noreply.github.com>
2023-03-21 19:22:11 +0800

    Init xray_config_manage.sh

---
+++ tool/xray_config_manage.sh
@@ -0,0 +1,45 @@
+
+#!/usr/bin/env bash
+
+# This script is used to manage xray configuration
+#
+# Dependencies: [jq]
+#
+# Author: zxcvos
+# Version: 0.1
+# Date: 2023-03-21
+
+declare configPath='/usr/local/etc/xray/config.json'
+declare matchTag='xray-script-xtls-reality'
+declare setPort=443
+declare setProto='vless'
+declare matchEmail='vless@xtls.reality'
+
+while [[ $# -ge 1 ]]; do
+  case $1 in
+  -t | --tag)
+    shift
+    [ "$1" ] || (echo 'Error: tag not provided' && exit 1)
+    matchTag="$1"
+    shift
+    ;;
+  -p | --port)
+    shift
+    [ "$1" ] || (echo 'Error: port not provided' && exit 1)
+    setPort="$1"
+    shift
+    ;;
+  -prcl | --protocol)
+    shift
+    [ "$1" ] || (echo 'Error: protocol not provided' && exit 1)
+    setProto="$1"
+    shift
+    ;;
+  -e | --email)
+    shift
+    [ "$1" ] || (echo 'Error: email not provided' && exit 1)
+    matchEmail="$1"
+    shift
+    ;;
+  esac
+done
